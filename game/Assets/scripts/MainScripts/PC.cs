e4f7666637d0191096949741e6f14bbd734f1d78
KotKot <mr-ed@o2.pl>
2014-06-03 13:54:21 +0200

    zmiana nazwy projektu, ustawienie sceny testowej, porzadek

---
+++ game/Assets/scripts/MainScripts/PC.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PC : Character {
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Run(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")); 
+		Jump(Input.GetKey(KeyCode.Space));
+	}
+}
---
+++ game/Assets/scripts/MainScripts/RM.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// In this class we have all settings like "speed", "forces" etc 
+/// </summary>
+public class RM : MonoBehaviour {
+	public static float playerSpeed = 0.17f; //speed of player
+	public static float playerJumpForce = 115f; //force of player jump
+
+}
